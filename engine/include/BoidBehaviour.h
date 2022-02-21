5326d56f0f36f8d9e3ed764e282d883d19df5fac
s1703955 <s1703955@glos.ac.uk>
2022-02-21 22:07:20 +0000

    flocking behaviours
    nice

--- engine/include/BoidBehaviour.h
+++ engine/include/BoidBehaviour.h
@@ -2,6 +2,7 @@
 #include "Component.h"
 #include "Transform.h"
 #include <glm.hpp>
+#include <vector>
 
 #ifndef BOID_BEHAVIOUR_H
 #define BOID_BEHAVIOUR_H
@@ -11,27 +12,25 @@ class BoidForce
 public:
 	BoidForce() : m_weight(1.0f) {};
 	~BoidForce() { delete next; next = nullptr; }
-	virtual glm::vec3 CalculateForce(Transform* a_transform, glm::vec3* a_data) = 0;
+	virtual glm::vec3 CalculateForce(Transform* a_transform, glm::vec3* a_data, std::vector<GameObject*>* a_neighbourPointer) = 0;
 	BoidForce* next;
 	float m_weight;
 };
-
-
 
 class Seek : public BoidForce
 {
 public:
 	Seek(bool a_flee) : m_bFlee(a_flee) {};
-	glm::vec3 CalculateForce(Transform* a_transform, glm::vec3* a_data) override;
-	bool m_bFlee;
+	glm::vec3 CalculateForce(Transform* a_transform, glm::vec3* a_data, std::vector<GameObject*>* a_neighbourPointer) override;
 	static glm::vec3 CalculateForce(glm::vec3 target, glm::vec3 pos, bool flee);
+	bool m_bFlee;
 };
 
 class Wander : public BoidForce
 {
 public:
 	Wander() {};
-	glm::vec3 CalculateForce(Transform* a_transform, glm::vec3* a_data) override;
+	glm::vec3 CalculateForce(Transform* a_transform, glm::vec3* a_data, std::vector<GameObject*>* a_neighbourPointer) override;
 	static glm::vec3 CalculateForce(const glm::vec3& a_pos, const glm::vec3& a_fwd, glm::vec3& a_wanderPoint,
 		const float length, const float radius, const float jitter);
 protected:
@@ -40,11 +39,23 @@ protected:
 	static float s_jitter;
 };
 
+class Flock : public BoidForce
+{
+public:
+	Flock() {};
+	glm::vec3 CalculateForce(Transform* a_transform, glm::vec3* a_data, std::vector<GameObject*>* a_neighbourPointer) override;
+	static float s_neighbourhoodRaidus;
+	static float s_weightAlignment;
+	static float s_weightSeperation;
+	static float s_weightCohesion;
+
+};
+
 enum BoidDataOffset
 {
 	TARGET,
 	WANDER,
-	COUNT
+	COUNT,
 };
 
 class BoidBehaviour : public Component
@@ -55,18 +66,20 @@ public:
 	void Update(float deltaTime) override;
 	void Draw(unsigned int a_shader) override {};
 	void Init() override;
+
+	inline void SetNeighbours(std::vector<GameObject*>* a_neighbours){	m_neighbours = a_neighbours;}
+
+	glm::vec3 GetVelocity() { return m_velocity; }
 protected:
 	glm::vec3 m_velocity;
 	static float s_maxVelocity;
 	static float s_speed;
 	Transform* p_transform;
 	glm::vec3 data[BoidDataOffset::COUNT];
+	std::vector<GameObject*>* m_neighbours;
 
 	
-	void SetTarget(glm::vec3 a_target)
-	{
-		data[BoidDataOffset::TARGET] = a_target;
-	}
+	void SetTarget(glm::vec3 a_target){data[BoidDataOffset::TARGET] = a_target;}
 public:
 	static void SetupForceList();
 };
--- engine/source/BoidBehaviour.cpp
+++ engine/source/BoidBehaviour.cpp
@@ -5,15 +5,22 @@
 float BoidBehaviour::s_speed = 1.0f;
 float BoidBehaviour::s_maxVelocity =3.0f;
 BoidForce* BoidBehaviour::s_head = nullptr;
-float Wander::s_jitter = 0.75f;
+float Wander::s_jitter = 1.0f;
 float Wander::s_radius = 2.0f;
-float Wander::s_length = 4.0f;
+float Wander::s_length = 3.0f;
+float Flock::s_neighbourhoodRaidus = 4.0f;
+float Flock::s_weightAlignment = 1.2f;
+float Flock::s_weightCohesion = 0.5f;
+float Flock::s_weightSeperation = 0.8f;
 
 void BoidBehaviour::Init()
 {
 	p_transform = m_owner->GetComponent<Transform>().get(); //fake warning honest
 	data[BoidDataOffset::TARGET] = glm::vec3(0,0,0);
 	data[BoidDataOffset::WANDER] = glm::vec3(0);
+	
+	m_neighbours = &GameObject::s_GameObjects;
+	//todo update this to use octals
 }
 
 void BoidBehaviour::SetupForceList()
@@ -21,8 +28,9 @@ void BoidBehaviour::SetupForceList()
 	//todo make this not stupid LOL
 	if (s_head == nullptr) {
 		s_head = new Seek(false); //seek
-		//s_head->next = new Seek(true); //flee
-		//s_head->next->next = new Wander(); //wander
+		s_head->next = new Seek(true); //flee
+		s_head->next->next = new Wander(); //wander
+		s_head->next->next->next = new Flock(); //flocking
 	}
 }	
 
@@ -30,10 +38,12 @@ void BoidBehaviour::Update(float deltaTime)
 {
 	//total forces for this frame
 	glm::vec3 totalForce(0); //calculate 
+
 	for (BoidForce* current = s_head; current != nullptr; current = current->next)
 	{
-		totalForce += current->CalculateForce(p_transform,data) * current->m_weight;
+		totalForce += current->CalculateForce(p_transform,data,m_neighbours) * current->m_weight;
 	}
+
 	glm::vec3 clamp(s_maxVelocity, 0, s_maxVelocity);
 	m_velocity += glm::clamp(totalForce * deltaTime * s_speed, -clamp, clamp ) ;
 
@@ -52,7 +62,7 @@ void BoidBehaviour::Update(float deltaTime)
 
 //////////////////////////////// Seek/Flee ///////////////////////////////////////////////////////
 
-glm::vec3 Seek::CalculateForce(Transform* a_transform, glm::vec3* a_data) //Seek / Flee force
+glm::vec3 Seek::CalculateForce(Transform* a_transform, glm::vec3* a_data, std::vector<GameObject*>* a_neighbourPointer) //Seek / Flee force
 {
 	return CalculateForce(a_data[BoidDataOffset::TARGET], a_transform->GetRow(MATRIX_ROW::POS_VEC), m_bFlee);
 }
@@ -66,7 +76,7 @@ glm::vec3 Seek::CalculateForce(glm::vec3 target, glm::vec3 pos, bool flee) //sta
 
 //////////////////////////////// Wander ///////////////////////////////////////////////////////
 
-glm::vec3 Wander::CalculateForce(Transform* a_transform, glm::vec3* a_data) //wander force
+glm::vec3 Wander::CalculateForce(Transform* a_transform, glm::vec3* a_data, std::vector<GameObject*>* a_neighbourPointer) //wander force
 {
 	return CalculateForce(a_transform->GetRow(MATRIX_ROW::POS_VEC), a_transform->GetRow(MATRIX_ROW::FWD_VEC),
 											  a_data[BoidDataOffset::WANDER], s_length, s_radius, s_jitter);
@@ -82,4 +92,42 @@ glm::vec3 Wander::CalculateForce(const glm::vec3& pos,const glm::vec3& fwd, glm:
 	wanderPoint = origin + (dir * radius) + glm::sphericalRand(jitter);
 
 	return Seek::CalculateForce(wanderPoint, pos, false);
+}
+
+//////////////////////////////// Flocking ///////////////////////////////////////////////////////
+
+glm::vec3 Flock::CalculateForce(Transform* a_transform, glm::vec3* a_data, std::vector<GameObject*>* a_neighbourPointer)
+{
+	unsigned int neighbourCount = 0;
+
+	glm::vec3 seperation(0);
+	glm::vec3 alignment(0);
+	glm::vec3 cohesion(0);
+
+	glm::vec3 localPos = a_transform->GetRow(MATRIX_ROW::POS_VEC);
+
+	for (GameObject* g : *a_neighbourPointer)
+	{
+		Transform* otherT = g->GetComponent<Transform>().get();
+		if (otherT == a_transform) break; //we are looking at ourselves get out early
+		glm::vec3 otherPos = otherT->GetRow(MATRIX_ROW::POS_VEC);
+
+		if (glm::length(localPos - otherPos) < s_neighbourhoodRaidus)
+		{
+			//boid is in our neighbourhood
+			neighbourCount++;
+			seperation += localPos - otherPos;
+			cohesion += otherPos;
+			alignment += g->GetComponent<BoidBehaviour>().get()->GetVelocity();	
+		}
+	}
+
+	if (neighbourCount == 0) return glm::vec3(0);
+
+	seperation = glm::normalize(seperation / neighbourCount) * s_weightSeperation;
+	alignment /= glm::normalize(alignment / neighbourCount) * s_weightAlignment;
+	cohesion /= glm::normalize (cohesion/neighbourCount - localPos) * s_weightCohesion;
+
+	return seperation + alignment + cohesion;
+
 }
